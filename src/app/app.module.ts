9e8dc06bcddb2e83d8292bf0195db3316c2eb658
DudsTheKid3756 <curlyq3756@gmail.com>
2022-12-23 07:51:36 -0500

    created issues component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { IssuesComponent } from "./issues/issues.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, IssuesComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: "", component: IssuesComponent }]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
